7d510f62183aeff1d0f78660a4fe6376d58de19e
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-07-21 21:01:09 +0200

    feat: add the posibility to add new tasks and get all the tasks

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Task = require('./models/task.model');
+
+app.use(express.json());
 
 mongoose.connect('mongodb+srv://admin:45c2q7U86owUKuDk@backenddb.pcvkind.mongodb.net/Express-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() =>  { 
@@ -13,4 +16,22 @@ mongoose.connect('mongodb+srv://admin:45c2q7U86owUKuDk@backenddb.pcvkind.mongodb
 
 app.get('/', (req, res) => {
     res.send('Hi from Node API');
+})
+
+app.get('/api/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  } 
+})
+
+app.post('/api/tasks', async (req, res) => {
+  try{
+    const task = await Task.create(req.body)
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 })
