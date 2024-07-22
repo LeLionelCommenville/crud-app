141a3d3bea26103ad4efe74f93e760d4898bb1d4
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-07-22 14:16:06 +0200

    feat: add the possibility to delete a task

--- index.js
+++ index.js
@@ -61,3 +61,16 @@ app.put('/api/task/:id', async (req, res) => {
     res.status(500).json({message: error.message});
   }
 })
+
+app.delete('/api/task/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if(!task) {
+      return res.status(404).json({message: 'Task not found'});
+    }
+    res.status(200).json({message: "Task deleted"});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
