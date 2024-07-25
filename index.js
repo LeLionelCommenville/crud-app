b575e87120e5b3a845b18cd233fb2d3d10420d5f
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-07-25 18:50:33 +0200

    archi: rework the code using controller and route instead of just index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const Task = require('./models/task.model');
+const app = express();
+const taskRoute = require('./routes/task.route.js');
+
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/tasks", taskRoute); 
+
 
 mongoose.connect('mongodb+srv://admin:45c2q7U86owUKuDk@backenddb.pcvkind.mongodb.net/Express-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() =>  { 
@@ -16,61 +22,4 @@ mongoose.connect('mongodb+srv://admin:45c2q7U86owUKuDk@backenddb.pcvkind.mongodb
 
 app.get('/', (req, res) => {
     res.send('Hi from Node API');
-})
-
-app.get('/api/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json(tasks);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  } 
-})
-
-app.post('/api/tasks', async (req, res) => {
-  try{
-    const task = await Task.create(req.body)
-    res.status(200).json(task);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-})
-
-app.get('/api/task/:id', async (req, res) => {
-  try {
-    const {id} = req.params;
-    const task = await Task.findById(req.params.id);
-    res.status(200).json(task);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-})
-
-app.put('/api/task/:id', async (req, res) => {
-  try {
-    const {id} = req.params;
-    const task = await Task.findByIdAndUpdate(id, req.body);
-    if(!task) {
-      return res.status(404).json({message: 'Task not found'});
-    }
-
-    const updatedTask =await Task.findById(id);
-    res.status(200).json(updatedTask);
-
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-})
-
-app.delete('/api/task/:id', async (req, res) => {
-  try {
-    const {id} = req.params;
-    const task = await Task.findByIdAndDelete(id);
-    if(!task) {
-      return res.status(404).json({message: 'Task not found'});
-    }
-    res.status(200).json({message: "Task deleted"});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
 })
